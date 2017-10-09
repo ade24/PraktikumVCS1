18fe77a59b822226008403f07a4451f2566a842c
ade24 <adeirpan32@gmail.com>
2017-10-09 11:26:29 +0700

    Initial commit

---
+++ app/src/main/java/mobpro/inf/praktikum/praktikumvcs1/MainActivity.java
@@ -0,0 +1,13 @@
+package mobpro.inf.praktikum.praktikumvcs1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
